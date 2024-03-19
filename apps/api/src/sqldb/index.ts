889ee0bd3fd9bbd12cee9fc026f4701a909ed0f3
Hieu Tran <hieutran.fu@gmail.com>
2024-03-19 22:33:37 +0700

    fix: pg pool should be instanciated once

--- apps/api/src/sqldb/index.ts
+++ apps/api/src/sqldb/index.ts
@@ -3,9 +3,11 @@ import { PostgresDialect } from "kysely";
 import { createSqlDb } from "@publiz/core";
 import { config } from "../config";
 
+const pool = new Pool(config.db);
+
 export const getSqlDb = () => {
   const dialect = new PostgresDialect({
-    pool: new Pool(config.db),
+    pool,
   });
 
   return createSqlDb(dialect);
